def75927a787b5d5b8560261bc74ca8c759b98b7
larry <492178939@qq.com>
2021-12-30 22:38:50 +0800

    基本导航

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-
+    <main-tab></main-tab>
   </div>
 </template>
 
 <script>
 
-
+import MainTab from "components/content/MainTab";
 export default {
   name: 'App',
   components: {
-
+    MainTab
   }
 }
 </script>
