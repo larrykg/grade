3a5dd6b6d0f717b6cfed67c0b75701678fc99f98
larry <492178939@qq.com>
2021-12-31 15:39:55 +0800

    路由基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <main-tab></main-tab>
+    <router-view/>
   </div>
 </template>
 
 <script>
 
 import MainTab from "components/content/MainTab";
+
 export default {
   name: 'App',
   components: {
     MainTab
-  }
+  },
+
+
 }
 </script>
 
