9b8896cacc90764f1a1b0b79994b631b52a07d28
larry <492178939@qq.com>
2022-01-05 16:43:46 +0800

    配置

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <main-tab/>
     <router-view/>
+    <back-top/>
   </div>
 </template>
 
 <script>
 
 import MainTab from "components/content/MainTab";
-
+import BackTop from "components/content/BackTop";
 export default {
   name: 'App',
   components: {
-    MainTab
+    MainTab,
+    BackTop
   }
 }
 </script>
