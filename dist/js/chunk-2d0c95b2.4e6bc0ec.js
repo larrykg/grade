f9f810032625af9a0dcac0ba0a0bd2a379682a99
larry <492178939@qq.com>
2022-01-11 16:39:25 +0800

    打包文件

---
+++ dist/js/chunk-2d0c95b2.4e6bc0ec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c95b2"],{5991:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("p",[e._v("新闻动态")])])}],a={name:"NewsDynamic"},s=a,u=t("2877"),i=Object(u["a"])(s,c,r,!1,null,"d311c320",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0c95b2.4e6bc0ec.js.map
